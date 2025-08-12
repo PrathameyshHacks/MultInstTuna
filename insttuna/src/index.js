b7944d28c23c1cf54fec4d68fba1a4852cc5da6b
PrathameyshHacks <t220144@famt.ac.in>
2025-08-12 22:22:14 +0530

    Update index.js

--- insttuna/src/index.js
+++ insttuna/src/index.js
@@ -2,24 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(reg => console.log('Service Worker registered:', reg))
-      .catch(err => console.log('SW registration failed:', err));
-  });
-}
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals'; // Make sure this file exists
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // ✅ Register the service worker for PWA
-//serviceWorkerRegistration.register();
+serviceWorkerRegistration.register();
 
 // Optional: performance monitoring
 reportWebVitals();
